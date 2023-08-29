8455b8a239bc5f717e36642f26a3d097d4cf5cab
svennijhuis <sanijhuis@live.nl>
2023-08-29 15:07:43 +0200

    test

--- script/index.js
+++ script/index.js
@@ -132,6 +132,6 @@ try {
   window.addEventListener("resize", () => {
     // We execute the same script as before
     let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    body.documentElement.style.setProperty("--vh", `${vh}px`);
   });
 } catch (error) {}
